6342bfb51d59bb4143f1143888adffd395707100
xianskel <cianskel@gmail.com>
2016-04-27 10:54:36 +0100

    All Activities Added

---
+++ app/src/main/java/com/example/xianskel/dreamalyze/ContactActivity.java
@@ -0,0 +1,45 @@
+package com.example.xianskel.dreamalyze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+
+public class ContactActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_contact){
+            return true;
+        }
+        else if(id == R.id.action_about){
+            return true;
+        }
+        else if(id == R.id.action_clear_logs){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}

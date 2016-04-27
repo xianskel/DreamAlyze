56c51efa22edf57b83430046414074e96a190d91
xianskel <cianskel@gmail.com>
2016-04-27 13:17:41 +0100

    Updated Views and Calander with Toast

--- app/src/main/java/com/example/xianskel/dreamalyze/SelectDateActivity.java
+++ app/src/main/java/com/example/xianskel/dreamalyze/SelectDateActivity.java
@@ -6,10 +6,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class SelectDateActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private CalendarView calendar;
+    private Button select_date_button;
+    private int selectedYear;
+    private int selectedMonth;
+    private int selectedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,30 @@ public class SelectDateActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        calendar = (CalendarView) findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view,
+                                            int year, int month, int dayOfMonth) {
+                selectedYear = year;
+                selectedMonth = month;
+                selectedDay = dayOfMonth;
+            }
+        });
+        select_date_button = (Button) findViewById(R.id.select_date_btn);
+
+        select_date_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Toast.makeText(getApplicationContext(),
+                        selectedDay + "/" + selectedMonth + "/" + selectedYear, Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

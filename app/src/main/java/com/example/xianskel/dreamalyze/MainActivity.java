2b735cd88be5f39cc9fba923b6e546b68c48f662
xianskel <cianskel@gmail.com>
2016-04-27 10:28:35 +0100

    Updated Screens

--- app/src/main/java/com/example/xianskel/dreamalyze/MainActivity.java
+++ app/src/main/java/com/example/xianskel/dreamalyze/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.xianskel.dreamalyze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("It's working");
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.record_dream_button);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                        RecordDreamActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+       getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+       if(id == R.id.action_contact){
+           return true;
+       }
+       else if(id == R.id.action_about){
+           return true;
+       }
+       else if(id == R.id.action_clear_logs){
+           return true;
+       }
+
+        return super.onOptionsItemSelected(item);
     }
 }

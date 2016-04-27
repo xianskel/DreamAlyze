fab4bc9c1f546afa7cf2c6f14c3776de16e116d1
Paul Ryan <paulryanpersonal@gmail.com>
2016-04-27 10:29:52 +0100

    Added dream file

---
+++ app/src/main/java/com/example/xianskel/dreamalyze/Dream.java
@@ -0,0 +1,8 @@
+package com.example.xianskel.dreamalyze;
+
+/**
+ * Created by Paul on 26/04/2016.
+ */
+public class Dream {
+
+}
--- app/src/main/java/com/example/xianskel/dreamalyze/MainActivity.java
+++ app/src/main/java/com/example/xianskel/dreamalyze/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.xianskel.dreamalyze;
-
+//API KEY IS c9708cb2f12b7e678d807d443ba61ca4b6890145
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("It's working");
+
     }
 }
